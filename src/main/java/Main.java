a0bb52df2b200562cf7115447c0d76759e0869ee
Adrian <adrian.jasieniecki0@gmail.com>
2022-05-22 12:46:30 +0200

    counting letters and sort

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
-import java.util.Locale;
+import java.util.*;
 
 public class Main {
+    static Map<String, Integer> letterCount = new HashMap<>();
     public static void main(String[] args) {
         StringBuilder buildString = new StringBuilder();
         for(String s : args) {
@@ -10,5 +11,26 @@ public class Main {
                 .toLowerCase(Locale.ROOT)
                 .replace(" ","")
                 .split("");
+
+        letterCounter(resultString);
+    }
+
+    static void letterCounter(String[] input) {
+        int counter = 1;
+        for(int i = 1 ; i < input.length ; i++) {
+            if(input[i].equals(input[0])) counter++;
+        }
+        letterCount.put(input[0], counter);
+        String[] newInput = Arrays.toString(input).replace(input[0],"").split("");
+        if(newInput.length>0) {
+            letterCounter(newInput);
+        } else {
+            displayResults();
+        }
+    }
+
+    static void displayResults() {
+        Map<String,Integer> sortedMap = new TreeMap<>(letterCount);
+        System.out.println(sortedMap);
     }
 }
